2a60ac6c63155e7f8b48d4400e30db49b92b8491
stthaeli <stthaeli@bx.fallmerayer.it>
2019-10-04 11:39:43 +0200

    Subserver Updates nr4

--- src/TCP_UDP_Server_Client/Server/Subserver2.java
+++ src/TCP_UDP_Server_Client/Server/Subserver2.java
@@ -1,6 +1,5 @@
 package TCP_UDP_Server_Client.Server;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -27,28 +26,6 @@ public class Subserver2 {
             e.printStackTrace();
         }
     }
-    private static String getDiskSpace(String command) {
-        try {
-            String[] driveLetter = command.split(" ");
-            driveLetter[1].replace(":", "");
-            if(driveLetter[1] == null || driveLetter[1] == " " || driveLetter[1] == "") { //check if any driveletter is given
-                return "Drive didn't found";
-            }
-            File file = new File(driveLetter[1] + ":\\");
-            long totalSpace = file.getTotalSpace(); //total disk space in bytes.
-            long usableSpace = file.getUsableSpace(); ///unallocated / free disk space in bytes.
-            long freeSpace = file.getFreeSpace(); //unallocated / free disk space in bytes.
-
-            return " === Partition Detail === \n" + "Total size : " + totalSpace /1024 /1024 + " mb \n" + "Space free : " + usableSpace /1024 /1024 + " mb \n" + "Space free : " + freeSpace /1024 /1024 + " mb \n";
-        }
-        catch (ArrayIndexOutOfBoundsException e) {
-            return "Drive didn't found";
-        }
-        catch (IndexOutOfBoundsException e) {
-            return "Drive didn't found";
-        }
-        //return  "Error in DiskSpace method";
-    }
     public static String getRamUsage() { //method to get actual memory usage
         Runtime runtime = Runtime.getRuntime();
         NumberFormat format = NumberFormat.getInstance();
@@ -66,12 +43,9 @@ public class Subserver2 {
         return sb.toString();
     }
     private static String execute_command(String command) {
-            command = command.split(" ")[0];
-        if (command.contains(prefix + "diskSpace")) return getDiskSpace(command); //return free disk space,
-        else if(command.contains(prefix + "threads")) return Integer.toString(java.lang.Thread.activeCount());
+        if(command.contains(prefix + "threads")) return Integer.toString(java.lang.Thread.activeCount());
         else if(command.contains(prefix + "Ram")) return getRamUsage();
         else if (command.contains(prefix + "stop")) System.exit(0);
-        else if (command.contains(prefix + "whoAmI")) return "senn";
         return null;
     }
 }
